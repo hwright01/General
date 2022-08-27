c37f860fc98cd4bdd64ece23a8a83019b9a90a7b
Harry Wright <harry.a.r.wright@gmail.com>
2022-08-27 13:48:13 +0100

    general

--- LCode/EULER/P10.py
+++ LCode/EULER/P10.py
@@ -1,30 +1,10 @@
-def addprimelist(p):
-    p = [2] if p == [] else p
-    i = p[-1]
-    while True:
+def addprimelist(n):
+    p = [2]
+    i = 3
+    while i < n:
         if not any([i % x == 0 for x in p]):
             p.append(i)
-            return(p)
         i = i + 1
-    
+    return sum(p)
 
-def largestfactor(n):
-    p = [2]
-    pfactors = []
-    i = 0
-    while True:
-        if n % p[i] == 0:
-            pfactors.append(p[i])
-            if max(pfactors) >= n:
-                return int(max(pfactors))
-            n = n / p[i]
-            i = -1
-        i = i + 1
-        if n in p:
-            return int(n)
-        if i == len(p):
-            if max(p) > n ** 0.5:
-                return int(n)
-            p = addprimelist(p)
-
-print(largestfactor(11111111111))
+print(addprimelist(2000000))
