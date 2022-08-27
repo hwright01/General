1bebf8fc10c3712ae3d82100de3b7ae4ab380e42
Harry Wright <harry.a.r.wright@gmail.com>
2022-08-27 16:52:25 +0100

    Finished P21

--- LCode/EULER/P21.py
+++ LCode/EULER/P21.py
@@ -1,12 +1,15 @@
-def factor_list(n):
+def factor_list_max(n):
     factors = [{0}, {1}]
     num = 1
+    redo = True
     while num < n:
         i = int(num/2)
         factors.append({1})
         while i >= num ** 0.5:
-            if num % i == 0 and i not in factors[num]:
+            if num % i == 0:
                 facs = set(factors[i])
+                if num == 220:
+                    pass
                 facs.update({i, int(num/i)})
                 try:
                     facs.remove(num)
@@ -15,7 +18,22 @@ def factor_list(n):
                 factors[num].update(facs)
             i = i - 1
         num = num + 1
+        if num == n and redo:
+            n = max([sum(x) for x in factors])
+            redo = False
     return factors
 
-a = factor_list(10000)[100]
-print(a)
+
+def amicable(n):
+    result = []
+    sums = [sum(x) for x in factor_list_max(n)]
+    for ind, sumval in enumerate(sums):
+        if ind >= n:
+            return set(result)
+        if sums[sumval] == ind and sumval != ind:
+            if sumval >= n:
+                result.append(ind)
+            else:
+                result = result + [sumval, ind]
+
+print(sum(amicable(10000)))
