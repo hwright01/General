3132669635f6e69a90609cff891bd822d7ae3a0a
Harry Wright <harry.a.r.wright@gmail.com>
2022-08-28 13:11:16 +0100

    Almost done CodeForces 706B

--- LCode/CODEF/706B.py
+++ LCode/CODEF/706B.py
@@ -1,9 +1,24 @@
-def binary_search(num,arr):
+def binary_search(arr,num):
+    total = 0
+    if len(arr) <= 2:
+        return sum([int(num >= x) for x in arr])
     mid = len(arr) // 2
-    
-    return
+    if arr[mid] == num:
+        i = 1
+        while arr[mid+i] == num:
+            i = i + 1
+        return mid + i
+    elif arr[mid] < num:
+        return mid + 1 + binary_search(arr[mid+1:], num)
+    else:
+        return binary_search(arr[:mid], num)
 
 input()
 shops = sorted([int(x) for x in input().split()])
-for i in range(int(input)):
-    print(binary_search(shops, int(input())))
+out = []
+
+for i in range(int(input())):
+    out.append(int(input()))
+
+for num in out:
+    print(binary_search(shops, num))
